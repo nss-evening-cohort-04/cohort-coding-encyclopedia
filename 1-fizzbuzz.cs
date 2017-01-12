891a1ff146621d752f979e9d3f63c12d6dea049e
Kate <katerebekah@gmail.com>
2017-01-11 20:28:46 -0600

    first challenge January 17

---
+++ 1-fizzbuzz.cs
@@ -0,0 +1,14 @@
+public class FizzBuzz
+{
+    /*
+        Challenge: Fill in the method below that prints the numbers from 1 to the limit being passed into the method. For multiples of three print “Fizz” instead of the number, and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+
+        Helpful Hints: Instead of console.log(), use Console.WriteLine() to "print" each number. Consider implementing this in a separate .NET app and then just paste the working code here.
+    */
+
+
+    public void GenerateFizzBuzz(int numberLimit) //how high the generator should count
+    {
+        //Your code goes here.
+    }
+}
