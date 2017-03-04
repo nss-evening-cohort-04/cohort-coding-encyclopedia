98322b7b709262ae173ee48f7e2c702c5b4eb069
Kate <katerebekah@gmail.com>
2017-03-04 10:06:13 -0600

    more arrays

--- 5-Arrays.cs
+++ 5-Arrays.cs
@@ -7,6 +7,9 @@ namespace ArrayChecker
 
         Given an array with integers from 1 - 100 with one number twice,
         make a method in your class that can determine which number is repeated.
-        
+
+        Given an array of numbers from 0 - 9 with any number of repeats, make a
+        method to find out which number occurs in the array most often.
+
      */
 }
