e15ae7caf0fbed07b2f4b6c2236da21093591ecf
Kate <katerebekah@gmail.com>
2017-03-04 09:55:10 -0600

    more challenges

---
+++ 9-Exceptions.cs
@@ -0,0 +1,10 @@
+namespace ExceptionHandling
+{
+    /*
+
+        You are building an application for Zoolandia, and you want to make a custom Exception for when an Animal is placed in the wrong Habitat.
+
+        You job here is to just write the custom exception. It should be named WrongHabitatException and should give a meaningful message.
+
+     */
+}
